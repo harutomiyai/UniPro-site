ef6442ce5ae88bfd5d4ea104f243851afd2771b7
Yuito Akatsuki <yuito@yuito-it.jp>
2024-11-04 08:57:31 +0900

    :bug: Implement responsive slideshow with dynamic image sizing and add custom hook for window size

--- src/components/slideshow.tsx
+++ src/components/slideshow.tsx
@@ -13,17 +13,44 @@ type PropsType = {
     images: string[];
 };
 
+import { useWindowSize } from '@/getWindowSize';
+
 export default function Slideshow({ images }: PropsType): JSX.Element {
-    const slideSettings = {
-        0: {
-            slidesPerView: 1.4,
-            spaceBetween: 10,
-        },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-        },
-    };
+    const res = useWindowSize();
+    const width = res[0];
+    const height = res[1];
+    let slideSettings = {};
+    if (width < 769) {
+        console.log("Mobile");
+
+        slideSettings = {
+            0: {
+                slidesPerView: 0,
+                spaceBetween: 10,
+            },
+            1024: {
+                slidesPerView: 0,
+                spaceBetween: 10,
+            },
+        };
+    } else {
+        console.log("Desktop");
+
+        slideSettings = {
+            0: {
+                slidesPerView: 1.4,
+                spaceBetween: 10,
+            },
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+        };
+    }
+    let imageStyle = "";
+    if (width > 768) {
+        imageStyle = styles.slideImage;
+    }
     return (
         <Swiper
             modules={[Navigation, Pagination, Autoplay]}
@@ -42,17 +69,17 @@ export default function Slideshow({ images }: PropsType): JSX.Element {
             pagination={{
                 clickable: true,
             }}
-            className={styles.slideWrapper + "w-full max-w-full"}
+            className={styles.slideWrapper + "w-full h-1/5"}
         >
             {images.map((src: string, index: number) => (
                 <SwiperSlide key={index}>
                     <Image
                         src={src}
                         alt="Slider Image"
-                        width={1024}
-                        height={768}
-                        sizes="(min-width: 1024px) 100vw, 60vw"
-                        className={styles.slideImage}
+                        width={width}
+                        height={height / 5}
+                        sizes="(min-width: 1024px) 100vw, 60vw h-1/5"
+                        className={imageStyle}
                     />
                 </SwiperSlide>
             ))}
