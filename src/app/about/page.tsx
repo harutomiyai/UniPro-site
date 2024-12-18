d920cb3d8b8f91c807b1f905e17864f11104e24d
唯人(Yuito Akatsuki) <yuito_it@outlook.jp>
2024-12-18 21:17:53 +0900

    Fix/layout (#92)
    * :bug: 不要な文字を削除してコードをクリーンアップ
    
    * :bug: ボタンコンポーネントのクラス名を修正し、参加フォームへのボタンのレイアウトを改善

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -67,13 +67,13 @@ const Home = () => {
   return (
     <main className="flex min-h-screen w-full flex-col items-center bg-white text-black space-y-0">
       <ReactMarkdown remarkPlugins={[remarkGfm]} className='markdown'>{markdownString}</ReactMarkdown>
-      <section id="join" className="flex space-y-3 items-center min-w-full p-20 flex-col bg-gray-100">
+      <section id="join" className="flex space-y-3 items-center min-w-full lg:p-20 p-10 flex-col bg-gray-100">
         <h3 className="text-2xl">
           <a href="#join">参加しよう！</a>
         </h3>
         <div className="text-center flex flex-col space-y-3">
           <p>UniProjectに参加するには、以下のリンクから参加フォームにアクセスしてください。<br />また、Discordは登録しなくても普段の活動の様子を見たり、メンバーと話したりできます。</p>
-          <div className='flex lg:flex-row flex-col justify-center items-center space-x-5'>
+          <div className='flex lg:flex-row flex-col justify-center items-center lg:space-x-5 lg:space-y-0 space-y-3'>
             <Button<'Link'> href="https://forms.gle/VsRQQ4LwnxvDUyH58" className="lg:w-2/5 w-full" disabled={false}>
               参加フォーム
             </Button>
@@ -81,7 +81,7 @@ const Home = () => {
               Discord
             </Button>
           </div>
-        </div>Ï
+        </div>
       </section>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,11 +51,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center bg-white text-black space-y-0">
       <Slideshow images={images} />
-      <section id="first" className="flex space-y-3 w-full items-center p-20 flex-col bg-gray-100">
+      <section id="first" className="flex space-y-3 w-full items-center lg:p-20 p-10 flex-col bg-gray-100">
         <div className='flex flex-col items-center space-y-1'>
           <div className="mb-4">
             <a href="#first">
-              <h3 className="text-2xl text-center">全国ネットのデジタル創作サークル</h3>
+              <h3 className="text-xl lg:text-2xl text-center">全国ネットのデジタル創作サークル</h3>
               <h2 className="text-6xl text-center">UniProject</h2>
             </a>
           </div>
@@ -64,7 +64,7 @@ export default function Home() {
             <br />
             活動内容
             <br />
-            Web開発/UTAU/デザイン/競技プログラミング/AI研究/映像制作/インフラ開発(サーバー構築など)/etc...
+            Web開発/UTAU/デザイン/競技プログラミング/AI/映像制作/インフラ開発(サーバー関連等)/etc...
             <br />
           </p>
           <Button<'button'> href="/about" disabled={false}>
@@ -89,13 +89,13 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section id="join" className="flex space-y-3 items-center min-w-full p-20 flex-col bg-gray-100">
+      <section id="join" className="flex space-y-3 items-center min-w-full lg:p-20 p-10 flex-col bg-gray-100">
         <h3 className="text-2xl">
           <a href="#join">参加しよう！</a>
         </h3>
         <div className="text-center flex flex-col space-y-3">
           <p>UniProjectに参加するには、以下のリンクから参加フォームにアクセスしてください。<br />また、Discordは登録しなくても普段の活動の様子を見たり、メンバーと話したりできます。</p>
-          <div className='flex lg:flex-row flex-col justify-center items-center space-x-5'>
+          <div className='flex lg:flex-row flex-col justify-center items-center lg:space-x-5 lg:space-y-0 space-y-3'>
             <Button<'Link'> href="https://forms.gle/VsRQQ4LwnxvDUyH58" className="lg:w-2/5 w-full" disabled={false}>
               参加フォーム
             </Button>
