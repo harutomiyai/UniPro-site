472960761f98e2e7f5a307cde10096743331f414
ayane0857 <tailangrongyi@gmail.com>
2024-12-22 16:57:23 +0900

    スライドショーの画像にリンクをつけました (#98)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,83 @@
-import { Metadata } from 'next';
-import { Button } from '@/components/button';
-import TwitterTimeline from '@/components/twitter-timeline';
-import { getRecentPosts } from '@/lib/posts';
-import PostList from '@/components/PostList';
+import { Metadata } from "next";
+import { Button } from "@/components/button";
+import TwitterTimeline from "@/components/twitter-timeline";
+import { getRecentPosts } from "@/lib/posts";
+import PostList from "@/components/PostList";
 
 export const metadata: Metadata = {
-  title: '全国ネットのデジタル創作サークル - UniProject',
+  title: "全国ネットのデジタル創作サークル - UniProject",
   description:
-    '全国ネットのデジタル創作サークル『UniProject』の公式サイトです！普段の活動や、その他お知らせなどをお届けします。',
+    "全国ネットのデジタル創作サークル『UniProject』の公式サイトです！普段の活動や、その他お知らせなどをお届けします。",
   twitter: {
-    card: 'summary',
-    site: '@UniPro_digital',
-    title: '全国ネットのデジタル創作サークル - UniProject',
-    description: "全国ネットのデジタル創作サークル『UniProject』の公式サイトです！普段の活動や、その他お知らせなどをお届けします。",
+    card: "summary",
+    site: "@UniPro_digital",
+    title: "全国ネットのデジタル創作サークル - UniProject",
+    description:
+      "全国ネットのデジタル創作サークル『UniProject』の公式サイトです！普段の活動や、その他お知らせなどをお届けします。",
     images: [
       {
-        url: 'https://www.uniproject.jp/img/UniPro_Logo.webp',
+        url: "https://www.uniproject.jp/img/UniPro_Logo.webp",
         width: 128,
         height: 134,
-        alt: 'UniProjectのロゴ',
-      },
-    ],
+        alt: "UniProjectのロゴ"
+      }
+    ]
   },
   openGraph: {
-    type: 'website',
-    url: 'https://www.uniproject.jp',
-    title: '全国ネットのデジタル創作サークル - UniProject',
-    description: '全国ネットのデジタル創作サークル『UniProject』の公式サイトです！普段の活動や、その他お知らせなどをお届けします。',
+    type: "website",
+    url: "https://www.uniproject.jp",
+    title: "全国ネットのデジタル創作サークル - UniProject",
+    description:
+      "全国ネットのデジタル創作サークル『UniProject』の公式サイトです！普段の活動や、その他お知らせなどをお届けします。",
     images: [
       {
-        url: 'https://www.uniproject.jp/img/UniPro_Logo.webp',
+        url: "https://www.uniproject.jp/img/UniPro_Logo.webp",
         width: 128,
         height: 134,
-        alt: 'UniProjectのロゴ',
-      },
-    ],
+        alt: "UniProjectのロゴ"
+      }
+    ]
   },
   alternates: {
-    canonical: "https://www.uniproject.jp/",
-  },
+    canonical: "https://www.uniproject.jp/"
+  }
 };
 
-import Slideshow from '@/components/slideshow';
+import Slideshow from "@/components/slideshow";
 
 export default function Home() {
   const images = [
-    "/img/callboy.webp",
-    "/img/fixer.webp",
-    "/img/UniPro-GitTool.webp",
-    "/img/arane.webp",
+    {
+      src: "/img/callboy.webp",
+      url: "https://www.nicovideo.jp/watch/sm43837772"
+    },
+    {
+      src: "/img/fixer.webp",
+      url: "https://www.nicovideo.jp/watch/sm43126973"
+    },
+    {
+      src: "/img/UniPro-GitTool.webp",
+      url: "https://github.com/UniPro-tech/UniPro-Git-Tool"
+    },
+    {
+      src: "/img/arane.webp",
+      url: "https://arane.uniproject.jp/"
+    }
   ];
-  const posts = getRecentPosts('announce', 4);
+  const posts = getRecentPosts("announce", 4);
   return (
     <main className="flex min-h-screen w-full flex-col items-center bg-white text-black space-y-0">
       <Slideshow images={images} />
-      <section id="first" className="flex space-y-3 w-full items-center lg:p-20 p-10 flex-col bg-gray-100">
-        <div className='flex flex-col items-center space-y-1'>
+      <section
+        id="first"
+        className="flex space-y-3 w-full items-center lg:p-20 p-10 flex-col bg-gray-100"
+      >
+        <div className="flex flex-col items-center space-y-1">
           <div className="mb-4">
             <a href="#first">
-              <h3 className="text-xl lg:text-2xl text-center">全国ネットのデジタル創作サークル</h3>
+              <h3 className="text-xl lg:text-2xl text-center">
+                全国ネットのデジタル創作サークル
+              </h3>
               <h2 className="text-6xl text-center">UniProject</h2>
             </a>
           </div>
@@ -70,36 +89,59 @@ export default function Home() {
             Web開発/UTAU/デザイン/競技プログラミング/AI/映像制作/インフラ開発(サーバー関連等)/etc...
             <br />
           </p>
-          <Button<'button'> href="/about" disabled={false}>
+          <Button<"button"> href="/about" disabled={false}>
             もっと詳しく
           </Button>
         </div>
       </section>
-      <section id="announce" className="flex flex-col space-y-3 items-center justify-center w-full p-20 bg-gray-200">
+      <section
+        id="announce"
+        className="flex flex-col space-y-3 items-center justify-center w-full p-20 bg-gray-200"
+      >
         <h3 className="text-2xl">
           <a href="#announce">最新情報</a>
           <p>UniProjectの最新情報をお届けします。</p>
         </h3>
         <div className="flex flex-col space-y-3 lg:flex-row lg:space-x-3">
           <div className="flex flex-col w-87.5 divide-y border-black divide-black">
-            <PostList dirname='announce' posts={posts} className='h-[500px] w-full' btnClassName='w-full' />
+            <PostList
+              dirname="announce"
+              posts={posts}
+              className="h-[500px] w-full"
+              btnClassName="w-full"
+            />
           </div>
           <div className="w-87.5">
             <TwitterTimeline />
           </div>
         </div>
       </section>
-      <section id="join" className="flex space-y-3 items-center min-w-full lg:p-20 p-10 flex-col bg-gray-100">
+      <section
+        id="join"
+        className="flex space-y-3 items-center min-w-full lg:p-20 p-10 flex-col bg-gray-100"
+      >
         <h3 className="text-2xl">
           <a href="#join">参加しよう！</a>
         </h3>
         <div className="text-center flex flex-col space-y-4">
-          <p>UniProjectに参加するには、以下のリンクから参加フォームにアクセスしてください。<br />また、Discordは登録しなくても普段の活動の様子を見たり、メンバーと話したりできます。</p>
-          <div className='flex lg:flex-row flex-col justify-center items-center lg:space-x-5 lg:space-y-0 space-y-3'>
-            <Button<'Link'> href="https://forms.gle/VsRQQ4LwnxvDUyH58" className="lg:w-2/5 w-full" disabled={false}>
+          <p>
+            UniProjectに参加するには、以下のリンクから参加フォームにアクセスしてください。
+            <br />
+            また、Discordは登録しなくても普段の活動の様子を見たり、メンバーと話したりできます。
+          </p>
+          <div className="flex lg:flex-row flex-col justify-center items-center lg:space-x-5 lg:space-y-0 space-y-3">
+            <Button<"Link">
+              href="https://forms.gle/VsRQQ4LwnxvDUyH58"
+              className="lg:w-2/5 w-full"
+              disabled={false}
+            >
               参加フォーム
             </Button>
-            <Button<'Link'> href="/discord" className="lg:w-2/5 w-full" disabled={false}>
+            <Button<"Link">
+              href="/discord"
+              className="lg:w-2/5 w-full"
+              disabled={false}
+            >
               Discord
             </Button>
           </div>
--- src/components/slideshow.tsx
+++ src/components/slideshow.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { JSX, useEffect, useState } from 'react';
+import React, { JSX, useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -10,129 +11,132 @@ import "swiper/css/pagination";
 import styles from "./index.module.css";
 
 type PropsType = {
-    images: string[];
+  images: {
+    src: string;
+    url: string;
+  }[];
 };
 const useIsPc = () => {
-    // 初期状態をサーバー側で false とする
-    const [isPc, setIsPc] = useState(false);
+  // 初期状態をサーバー側で false とする
+  const [isPc, setIsPc] = useState(false);
 
-    useEffect(() => {
-        const mediaQueryList = window.matchMedia("(min-width: 1024px)");
-        const listener = () => setIsPc(mediaQueryList.matches);
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia("(min-width: 1024px)");
+    const listener = () => setIsPc(mediaQueryList.matches);
 
-        // 初回に現在の状態を設定
-        setIsPc(mediaQueryList.matches);
-        mediaQueryList.addEventListener("change", listener);
+    // 初回に現在の状態を設定
+    setIsPc(mediaQueryList.matches);
+    mediaQueryList.addEventListener("change", listener);
 
-        return () => mediaQueryList.removeEventListener("change", listener);
-    }, []);
+    return () => mediaQueryList.removeEventListener("change", listener);
+  }, []);
 
-    return isPc;
+  return isPc;
 };
 
 function Mobile({ images }: PropsType): JSX.Element {
-    console.log("mobile");
-    const slideSettings = {
-        0: {
-            slidesPerView: 0,
-            spaceBetween: 1,
-        },
-        1024: {
-            slidesPerView: 0,
-            spaceBetween: 1,
-        },
-    };
-    const imageStyle = "";
-    return (
-        <Swiper
-            modules={[Navigation, Pagination, Autoplay]}
-            breakpoints={slideSettings}
-            slidesPerView={'auto'}
-            centeredSlides={true}
-            mousewheel={true}
-            freeMode={true}
-            loop={true}
-            speed={1000}
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }}
-            navigation
-            pagination={{
-                clickable: true,
-            }}
-            className={styles.slideWrapper + "w-full max-w-full"}
-        >
-            {images.map((src: string, index: number) => (
-                <SwiperSlide key={index}>
-                    <Image
-                        src={src}
-                        alt="Slider Image"
-                        width={1024}
-                        height={0}
-                        sizes="(min-width: 1024px) 100vw, 60vw"
-                        className={imageStyle}
-                    />
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    )
+  console.log("mobile");
+  const slideSettings = {
+    0: {
+      slidesPerView: 0,
+      spaceBetween: 1
+    },
+    1024: {
+      slidesPerView: 0,
+      spaceBetween: 1
+    }
+  };
+  const imageStyle = "";
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      breakpoints={slideSettings}
+      slidesPerView={"auto"}
+      centeredSlides={true}
+      mousewheel={true}
+      freeMode={true}
+      loop={true}
+      speed={1000}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false
+      }}
+      navigation
+      pagination={{
+        clickable: true
+      }}
+      className={styles.slideWrapper + "w-full max-w-full"}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <Link href={image.url}>
+            <Image
+              src={image.src}
+              alt="Slider Image"
+              width={1024}
+              height={0}
+              sizes="(min-width: 1024px) 100vw, 60vw"
+              className={imageStyle}
+            />
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
 
 const PC = ({ images }: PropsType) => {
-    console.log("pc");
-    const slideSettings = {
-        0: {
-            slidesPerView: 1.4,
-            spaceBetween: 10,
-        },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-        },
-    };
-    const imageStyle = styles.slideImage;
-    return (
-        <Swiper
-            modules={[Navigation, Pagination, Autoplay]}
-            breakpoints={slideSettings}
-            slidesPerView={'auto'}
-            centeredSlides={true}
-            mousewheel={true}
-            freeMode={true}
-            loop={true}
-            speed={1000}
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }}
-            navigation
-            pagination={{
-                clickable: true,
-            }}
-            className={styles.slideWrapper + "w-full max-w-full"}
-        >
-            {images.map((src: string, index: number) => (
-                <SwiperSlide key={index}>
-                    <Image
-                        src={src}
-                        alt="Slider Image"
-                        width={1024}
-                        height={0}
-                        sizes="(min-width: 1024px) 100vw, 60vw"
-                        className={imageStyle}
-                    />
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    )
-}
+  console.log("pc");
+  const slideSettings = {
+    0: {
+      slidesPerView: 1.4,
+      spaceBetween: 10
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    }
+  };
+  const imageStyle = styles.slideImage;
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      breakpoints={slideSettings}
+      slidesPerView={"auto"}
+      centeredSlides={true}
+      mousewheel={true}
+      freeMode={true}
+      loop={true}
+      speed={1000}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false
+      }}
+      navigation
+      pagination={{
+        clickable: true
+      }}
+      className={styles.slideWrapper + "w-full max-w-full"}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <Link href={image.url}>
+            <Image
+              src={image.src}
+              alt="Slider Image"
+              width={1024}
+              height={0}
+              sizes="(min-width: 1024px) 100vw, 60vw"
+              className={imageStyle}
+            />
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
 export default function Slideshow({ images }: PropsType): JSX.Element {
-    const isPc = useIsPc();
-    return (
-        <>
-            {isPc ? <PC images={images} /> : <Mobile images={images} />}
-        </>
-    );
+  const isPc = useIsPc();
+  return <>{isPc ? <PC images={images} /> : <Mobile images={images} />}</>;
 }
