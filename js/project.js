781f59201cce7192757bbc8c64d509ebce047009
Haruto Miyai <195989610+harutomiyai@users.noreply.github.com>
2025-03-27 07:48:44 +0900

    feat: プロジェクトページのコンテンツを更新し、アニメーション効果を追加

--- js/project.js
+++ js/project.js
@@ -19,3 +19,49 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+
+const list = document.getElementById("word-list");
+const words = list.querySelectorAll("li");
+const wordDisplay = document.querySelector(".word-area");
+
+function update() {
+  const centerY = window.innerHeight / 1.1;
+  let closest = null;
+  let minDist = Infinity;
+
+  words.forEach(word => {
+    const rect = word.getBoundingClientRect();
+    const y = rect.top + rect.height / 1;
+    const dist = Math.abs(centerY - y);
+
+    if (dist < minDist) {
+      minDist = dist;
+      closest = word.textContent;
+    }
+  });
+
+  if (closest) {
+    wordDisplay.textContent = closest;
+  }
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const items = document.querySelectorAll(".ex_list li");
+        items.forEach(item => {
+          item.style.animationPlayState = "running";
+        });
+        observer.unobserve(entry.target); // 一回だけでOKなら
+      }
+    });
+  }, { threshold: 0.3 });
+  
+  const exList = document.querySelector(".ex_list");
+  observer.observe(exList);
+  
