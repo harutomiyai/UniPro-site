19cf0d8357809cc43f7a2b768a74508af445679b
Haruto Miyai <195989610+harutomiyai@users.noreply.github.com>
2025-03-26 17:21:24 +0900

    feat: プロジェクトページのレイアウトを追加し、サイドバーの機能を実装

---
+++ js/project.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.querySelector(".header__menu-icon");
+    const rightSidebar = document.getElementById("sidebarRight");
+
+    menuButton.addEventListener("click", () => {
+        rightSidebar.classList.toggle("active");
+    });
+
+
+    rightSidebar.addEventListener("click", () => {
+        rightSidebar.classList.toggle("active");
+    });
+
+    document.addEventListener("click", (event) => {
+        if (!rightSidebar.contains(event.target) && !menuButton.contains(event.target)) {
+            rightSidebar.classList.remove("active");
+        }
+    });
+
+});
+
